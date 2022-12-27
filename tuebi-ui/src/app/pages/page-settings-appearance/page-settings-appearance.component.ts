adbf6080e4c45a11c98a168a4d703212c59ec7a1
Nguyen Cong Cuong <cuongnc.fe@gmail.com>
2022-12-28 00:18:24 +0700

    [15] Appearance Settings does not work properly when toggling UI

--- tuebi-ui/src/app/pages/page-settings-appearance/page-settings-appearance.component.ts
+++ tuebi-ui/src/app/pages/page-settings-appearance/page-settings-appearance.component.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
-import { firstValueFrom, map, Observable, tap } from 'rxjs';
+import { firstValueFrom, map, Observable, of, tap } from 'rxjs';
 import { IconComponent } from '../../components/icon/icon.component';
 import { User } from '../../interfaces/user.interface';
 import { UserEntityService } from '../../services/user-entity.service';
 import { UserService } from '../../services/user.service';
+
+interface SettingItem {
+	title: string,
+	isActive: boolean,
+	onToggle: Function,
+	desc: string
+}
 
 @Component({
 	selector: 'app-settings-detail-appearance',
@@ -18,120 +25,76 @@ import { UserService } from '../../services/user.service';
 	templateUrl: './page-settings-appearance.component.html',
 	styleUrls: ['./page-settings-appearance.component.scss']
 })
-export class PageSettingsAppearanceComponent {
+export class PageSettingsAppearanceComponent implements OnInit {
 	user$ = new Observable<User>();
 	
-	// Default value, check API
 	isCompactModeOn = false;
-	isFaviconShown = true;
-	isBookmarkURLShorten = false;
-	isBookmarkCountShown = true;
-	isBookmarkUrlShown = true;
-	
-	items = [
-		{
-			title: 'Compact Mode',
-			isActive: this.isCompactModeOn,
-			onToggle: () => this.toggleCompactMode(),
-			desc: 'Can\'t see enough bookmarks? Let\'s use compact mode'
-		},
-		{
-			title: 'Show Favicon',
-			isActive: this.isFaviconShown,
-			onToggle: () => this.toggleFaviconShown(),
-			desc: 'Display an icon to the left of your bookmark URL'
-		},
-		{
-			title: 'Show Bookmark URL',
-			isActive: this.isBookmarkUrlShown,
-			onToggle: () => this.toggleBookmarkUrl(),
-			desc: 'Display the bookmark URL'
-		},
-		{
-			title: 'Shorten Bookmark URL',
-			isActive: this.isBookmarkURLShorten,
-			onToggle: () => this.toggleBookmarkURLShorten(),
-			desc: 'Hide \'http\', \'https\' at the beginning and \'/\' at the ending of your bookmark URL'
-		},
-		{
-			title: 'Show Bookmark Count',
-			isActive: this.isBookmarkCountShown,
-			onToggle: () => this.toggleBookmarkCount(),
-			desc: 'Display the total number of bookmarks for each category'
-		}
-	]
+	isFaviconShown= false;
+	isBookmarkUrlShown = false;
+	isBookmarkUrlShorten = false;
+	isBookmarkCountShown = false;
 	
 	constructor(
 		private userService: UserService,
 		private userEntityService: UserEntityService,
 	) {
-		this.user$ = this.userEntityService.entities$.pipe(
-			map(users => users[0]),
-			tap((user) => {
-				if (user) {
-					this.isCompactModeOn = user.user_settings.is_compact_mode_on;
-					this.isFaviconShown = user.user_settings.is_favicon_shown;
-					this.isBookmarkURLShorten = user.user_settings.is_bookmark_url_shorten;
-					this.isBookmarkCountShown = user.user_settings.is_bookmark_count_shown;
-					this.isBookmarkUrlShown = user.user_settings.is_bookmark_url_shown;
-				}
-			})
-		);
+		this.user$ = this.userEntityService.entities$.pipe(map(users => users[0]));
+	}
+	
+	public ngOnInit() {
+		this.user$.subscribe(user => {
+			const {
+					is_compact_mode_on,
+					is_favicon_shown,
+					is_bookmark_url_shown,
+					is_bookmark_url_shorten,
+					is_bookmark_count_shown
+			} = user.user_settings
+			
+			this.isCompactModeOn = is_compact_mode_on;
+			this.isFaviconShown = is_favicon_shown;
+			this.isBookmarkUrlShown = is_bookmark_url_shown;
+			this.isBookmarkUrlShorten = is_bookmark_url_shorten;
+			this.isBookmarkCountShown = is_bookmark_count_shown;
+		});
+	}
+	
+	async toggle(key: 
+			'is_compact_mode_on' | 
+			'is_favicon_shown' | 
+			'is_bookmark_url_shorten' | 
+			'is_bookmark_count_shown' | 
+			'is_bookmark_url_shown'
+	) {
+		const user = await firstValueFrom(this.user$);
+		
+		this.userEntityService.update({
+			id: user.id,
+			user_settings: {
+				...user.user_settings,
+				[key]: !user.user_settings[key]
+			}
+		});
 	}
 	
 	async toggleCompactMode() {
-		const user = await firstValueFrom(this.user$);
-		this.userEntityService.update({
-			id: user.id,
-			user_settings: {
-				...user.user_settings,
-				is_compact_mode_on: !user.user_settings.is_compact_mode_on
-			}
-		});
+		await this.toggle('is_compact_mode_on');	
 	}
 	
 	async toggleFaviconShown() {
-		const user = await firstValueFrom(this.user$);
-		this.userEntityService.update({
-			id: user.id,
-			user_settings: {
-				...user.user_settings,
-				is_favicon_shown: !user.user_settings.is_favicon_shown
-			}
-		});
+		await this.toggle('is_favicon_shown');
 	}
 	
-	async toggleBookmarkURLShorten() {
-		const user = await firstValueFrom(this.user$);
-		this.userEntityService.update({
-			id: user.id,
-			user_settings: {
-				...user.user_settings,
-				is_bookmark_url_shorten: !user.user_settings.is_bookmark_url_shorten
-			}
-		});
+	async toggleBookmarkUrlShown() {
+		await this.toggle('is_bookmark_url_shown');
 	}
 	
-	async toggleBookmarkCount() {
-		const user = await firstValueFrom(this.user$);
-		this.userEntityService.update({
-			id: user.id,
-			user_settings: {
-				...user.user_settings,
-				is_bookmark_count_shown: !user.user_settings.is_bookmark_count_shown
-			}
-		});
+	async toggleBookmarkUrlShorten() {
+		await this.toggle('is_bookmark_url_shorten');
 	}
 	
-	async toggleBookmarkUrl() {
-		const user = await firstValueFrom(this.user$);
-		this.userEntityService.update({
-			id: user.id,
-			user_settings: {
-				...user.user_settings,
-				is_bookmark_url_shown: !user.user_settings.is_bookmark_url_shown
-			}
-		});
+	async toggleBookmarkCountShown() {
+		await this.toggle('is_bookmark_count_shown');
 	}
 	
 }
