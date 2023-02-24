0353e2853bb8ba78bd79b0a9086929b3600c5c69
Nguyen Cong Cuong <cuongnc.fe@gmail.com>
2023-02-25 00:05:35 +0700

    [30] Navigate to Bookmark Dashboard when User Logins

--- tuebi-ui/src/app/app.component.ts
+++ tuebi-ui/src/app/app.component.ts
@@ -1,24 +1,86 @@
-import { Component, isDevMode, OnInit } from '@angular/core';
+import { Component, Inject, isDevMode, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService } from '@azure/msal-angular';
+import { EventMessage, EventType, InteractionStatus, RedirectRequest } from '@azure/msal-browser';
 import { Store } from '@ngrx/store';
+import { filter, Subject, takeUntil } from 'rxjs';
 import { BreakpointService } from 'src/app/services/breakpoint.service';
+import { routeList } from './enums/routes.enum';
 import { AppState } from './reducers';
+import { UserService } from './services/user.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+	private _destroying$ = new Subject<void>();
+	public isB2CLoggedIn = false;
+	public isCollapsed = false;
+	public routeList = routeList;
+	
 	constructor(
+		@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
 		private router: Router,
 		private route: ActivatedRoute,
 		private store: Store<AppState>,
 		private breakpointService: BreakpointService,
+		private msalService: MsalService,
+		private msalBroadcastService: MsalBroadcastService,
+		private userService: UserService,
 	) {
 		this.breakpointService.ngOnInit();
 	}
 	
 	ngOnInit(): void {
 		isDevMode() ? console.log('Development!') : console.log('Production!');
+		
+		this.msalBroadcastService.msalSubject$
+			.pipe(
+				filter((msg: EventMessage) => 
+					msg.eventType === EventType.LOGIN_SUCCESS ||
+					msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS ||
+					msg.eventType === EventType.SSO_SILENT_SUCCESS
+				),
+			)
+			.subscribe((result: EventMessage) => {
+				console.log('result', result)
+				
+				// Check database, if this is new user, create an initial user data.
+				const payload = result.payload as any;
+				localStorage.setItem('b2c_payload', JSON.stringify(payload))
+				this.userService.createOne(payload).subscribe();
+			});
+		
+		this.msalBroadcastService.inProgress$
+			.pipe(
+				filter((status: InteractionStatus) => status === InteractionStatus.None),
+				takeUntil(this._destroying$)
+			)
+			.subscribe(() => {
+				this.setLoginDisplay();
+				if(this.isB2CLoggedIn) this.router.navigateByUrl('space/categories');
+			})
+	}
+	
+	ngOnDestroy(): void {
+		this._destroying$.next(undefined);
+		this._destroying$.complete();
+	}
+	
+	login() {
+		if (this.msalGuardConfig.authRequest){
+			this.msalService.loginRedirect({...this.msalGuardConfig.authRequest} as RedirectRequest);
+		} else {
+			this.msalService.loginRedirect();
+		}
+	}
+	
+	logout() {
+		this.msalService.logoutRedirect();
+	}
+	
+	setLoginDisplay() {
+		this.isB2CLoggedIn = this.msalService.instance.getAllAccounts().length > 0;
 	}
 }
--- tuebi-ui/src/app/pages/page-home/page-home.component.ts
+++ tuebi-ui/src/app/pages/page-home/page-home.component.ts
@@ -1,60 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService } from '@azure/msal-angular';
-import { EventMessage, EventType, InteractionStatus, RedirectRequest } from '@azure/msal-browser';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { filter, Subject, takeUntil } from 'rxjs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { IconComponent } from '../../components/icon/icon.component';
 import { LogoComponent } from '../../components/logo/logo.component';
 import { featuresAtGlance, featuresEnum } from '../../enums/features.enum';
-import { ROUTE, routeList } from '../../enums/routes.enum';
+import { ROUTE } from '../../enums/routes.enum';
 import { themes } from '../../enums/theme.enum';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-page-home',
   standalone: true,
-  imports: [CommonModule, LogoComponent, RouterLinkActive, RouterLink, IconComponent, NzButtonModule],
+  imports: [CommonModule, LogoComponent, RouterLinkActive, RouterLink, IconComponent, NzButtonModule, NzIconModule, NzMenuModule, NzToolTipModule],
   templateUrl: './page-home.component.html',
   styleUrls: ['./page-home.component.scss']
 })
-export class PageHomeComponent implements OnInit, OnDestroy {
-  private readonly _destroying$ = new Subject<void>();
-  
-  public isB2CLoggedIn = false;
-  
-  constructor(
-    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
-    private msalService: MsalService,
-    private msalBroadcastService: MsalBroadcastService,
-    private userService: UserService
-  ) {}
-  
-  public ngOnInit() {
-    this.msalBroadcastService.msalSubject$
-      .pipe(
-        filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
-      )
-      .subscribe((result: EventMessage) => {
-        console.log('result', result)
-      
-        // Check database, if this is new user, create an initial user data.
-        const payload = result.payload as any;
-        localStorage.setItem('b2c_payload', JSON.stringify(payload))
-        this.userService.createOne(payload).subscribe();
-      });
-  
-    this.msalBroadcastService.inProgress$
-      .pipe(
-        filter((status: InteractionStatus) => status === InteractionStatus.None),
-        takeUntil(this._destroying$)
-      )
-      .subscribe(() => {
-        this.setLoginDisplay();
-      })
-  }
-  
+export class PageHomeComponent implements OnInit {
   title = 'tuebi.io';
   desc_1 = 'Your daily bookmark manager';
   desc_2 = 'Private. Minimal. Efficient';
@@ -63,33 +27,12 @@ export class PageHomeComponent implements OnInit, OnDestroy {
   currentUrl = '/';
   
   ROUTE = ROUTE;
-  isShown: boolean = false;
   features = featuresEnum;
   featuresAtGlance = featuresAtGlance;
-  routeList = routeList;
   
-  login() {
-    if (this.msalGuardConfig.authRequest){
-      this.msalService.loginRedirect({...this.msalGuardConfig.authRequest} as RedirectRequest);
-    } else {
-      this.msalService.loginRedirect();
-    }
-  }
+  constructor() {}
   
-  logout() {
-    this.msalService.logoutRedirect();
-  }
-  
-  setLoginDisplay() {
-    this.isB2CLoggedIn = this.msalService.instance.getAllAccounts().length > 0;
-  }
-  
-  ngOnDestroy(): void {
-    this._destroying$.next(undefined);
-    this._destroying$.complete();
-  }
-  
-  toggleNav(bool: boolean) {
-    this.isShown = !bool;
+  public ngOnInit() {
+
   }
 }
