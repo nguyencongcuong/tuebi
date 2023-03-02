30adc936917abbe88d85cc22211fb313488dd7d4
Nguyen Cong Cuong <cuongnc.fe@gmail.com>
2023-03-02 13:42:06 +0700

    [37] Add a Welcome API at Root Domain for Testing Purpose

--- tuebi-api/src/app.controller.ts
+++ tuebi-api/src/app.controller.ts
@@ -1,5 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { sendSuccess } from './utilities';
 
 @Controller('')
 export class AppController {
+  @Get()
+  welcome() {
+    return sendSuccess('Welcome to tuebi.io API')
+  }
 }
