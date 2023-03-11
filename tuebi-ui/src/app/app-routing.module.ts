ca3458fa3d49c50ffdf17fd23a62806d5786ea6f
Nguyen Cong Cuong <cuongnc.fe@gmail.com>
2023-03-11 21:44:29 +0700

    [55] Create A Privacy Policy Page

--- tuebi-ui/src/app/app-routing.module.ts
+++ tuebi-ui/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './components/page-home/page-home.component';
+import { PagePrivacyPolicyComponent } from './components/page-privacy-policy/page-privacy-policy.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: PageHomeComponent,
+	},
+	{
+		path: 'privacy-policy',
+		component: PagePrivacyPolicyComponent
 	}
 ];
 
---
+++ tuebi-ui/src/app/components/page-privacy-policy/page-privacy-policy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page-privacy-policy',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-privacy-policy.component.html',
+  styleUrls: ['./page-privacy-policy.component.scss']
+})
+export class PagePrivacyPolicyComponent {
+
+}
