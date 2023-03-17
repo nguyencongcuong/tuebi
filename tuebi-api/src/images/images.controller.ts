e676ca48264b72d55d7783404e01a50c862cd8ae
Nguyen Cong Cuong <cuongnc.fe@gmail.com>
2023-03-17 09:08:29 +0700

    [66] Update user avatar url after upload the avatar to Blob

--- tuebi-api/src/images/images.controller.ts
+++ tuebi-api/src/images/images.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Post, Request, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Logger, Post, Request, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AzureB2cJwt } from '../auth/guards/azure-b2c-jwt';
+import { UsersService } from '../users/users.service';
 import { sendError, sendSuccess } from '../utilities';
 import { ImagesService } from './images.service';
-import { Logger } from '@nestjs/common';
 
 @Controller('images')
 export class ImagesController {
   private logger = new Logger(ImagesController.name);
   
   constructor(
-    private readonly imagesService: ImagesService
+    private readonly imagesService: ImagesService,
+    private usersService: UsersService,
   ) {}
   
   @UseGuards(AzureB2cJwt)
@@ -23,10 +24,23 @@ export class ImagesController {
     try {
       const user = req.user;
       const url = await this.imagesService.upload(user.id, file.buffer);
+      console.log('url, ', url)
       const data = {
         id: user.id,
         user_avatar_url: url,
       };
+      this.usersService.updateOne({
+        id: user.id,
+        partition_key: '',
+        operations: [
+          {
+            op: 'add',
+            path: '/user_avatar_url',
+            value: url,
+          }
+        ],
+        
+      })
       return sendSuccess(data);
     } catch (e) {
       this.logger.error(e.message, this.uploadAvatar.name);
